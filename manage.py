9b17d6f91badd18cec69c02b26f85de7a995ef26
Skye <46286597+Skye-31@users.noreply.github.com>
2026-04-01 14:40:09 +0100

    Fix some bugs

--- manage.py
+++ manage.py
@@ -16,63 +16,67 @@ def create_unix_mammoth_group_if_not_exists():
     subprocess.run(command, check=True, shell=True)
     print("Ensured mammoth user group exists")
 
-# Function to create a unix user
+
+# Function to create a unix user. Expects sanitized input.
 def create_unix_user(username: str):
-    # generate a random password, no user will ever need to know their password
+    # Generate a random password. Users cannot know their password or switch users.
     random_pass = secrets.token_hex(32)
 
     # Create the user in the mammoth cluster users
-    command = f"sudo useradd {username} -p {random_pass} -g {MAMMOTH_USERS_GROUP}"
+    command = f"useradd {username} -p {random_pass} -g {MAMMOTH_USERS_GROUP}"
     subprocess.run(command, check=True, shell=True)
 
     print(f"User {username} created successfully!")
 
+
 # Function to list all unix users who are in the mammoth users group
 def list_mammoth_unix_users() -> list[str]:
-    users: list[str] = []
+    mammoth_users: list[str] = []
 
     # Get a list of users mapped to their groups in the format
     # "username : group1 group2"
     command = "cat /etc/passwd | awk -F':' '{ print $1}' | xargs -n1 groups"
-    result = subprocess.run(command, check=True, shell=True, capture_output=True, text=True)
-    users = result.stdout.splitlines()
+    result = subprocess.run(
+        command, check=True, shell=True, capture_output=True, text=True
+    )
+    user_lines = result.stdout.splitlines()
     print("List of users:")
-    for user_line in users:
+    for user_line in user_lines:
         # This user is part of the mammoth group
         if MAMMOTH_USERS_GROUP in user_line:
-            user = user_line.split(' ')[0]
-            users.append(user)
+            user = user_line.split(" ")[0]
+            mammoth_users.append(user)
 
-    return users
+    return mammoth_users
 
-# # Function to delete a user
+
+# Function to delete a user. Expects sanitized input.
 def delete_user(username):
-    command = f"sudo userdel -r {username}"  # -r flag removes the user'
+    command = f"userdel -r {username}"  # -r flag removes the user'
     # Use 'echo' to pass the password automatically
     subprocess.run(command, shell=True, check=True)
     print(f"User {username} deleted successfully!")
 
+
 # Sync the list of users to the cloudflare access policy
 def sync_cloudflare_access_policy(users: list[str]):
-    client = Cloudflare(
-        api_token=CLOUDFLARE_API_TOKEN
-    )
+    client = Cloudflare(api_token=CLOUDFLARE_API_TOKEN)
 
-    policy = client.zero_trust.access.policies.update(
+    client.zero_trust.access.policies.update(
         name="Automatically Managed User Policy",
         policy_id=CLOUDFLARE_ACCESS_POLICY_ID,
-        account_id=CLOUDFLARE_ACCESS_POLICY_ID,
-        include=[{
-            "email": {
-                "email": user + "@exeter.ac.uk"
-            }
-        } for user in users],
+        account_id=CLOUDFLARE_ACCOUNT_ID,
+        include=[{"email": {"email": user + "@exeter.ac.uk"}} for user in users],
         decision="allow",
     )
-    print(f"Updated access policy with id: {policy.id}")
+    print(f"Successfully updated access policy")
 
 
 def main():
+    if os.geteuid() != 0:
+        print("This script must be run as root")
+        exit(1)
+
     if CLOUDFLARE_API_TOKEN == None or len(CLOUDFLARE_API_TOKEN) == 0:
         print("Specify a CLOUDFLARE_API_TOKEN environment variable")
         exit(1)
@@ -80,7 +84,7 @@ def main():
     create_unix_mammoth_group_if_not_exists()
 
     # Todo: Get list of users from somewhere
-    new_users = ['sr996']
+    new_users = ["sr996"]
 
     # Get existing users
     existing_users = list_mammoth_unix_users()
@@ -99,6 +103,7 @@ def main():
 
     # Sync the users to the cloudflare access policy
     sync_cloudflare_access_policy(new_users)
+
 
 if __name__ == "__main__":
     main()
